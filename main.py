ea04acd23121f0c9a8ce2d5ee730b53d9fb528e7
Nearground <54041291+Nearground@users.noreply.github.com>
2025-07-29 19:00:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with sync_playwright() as p:
         browser = browser_type.launch(headless=False, slow_mo=1000)
         page = browser.new_page()
         page.goto('https://chat.openai.com/auth/login')
-        age.locator("Log In:).wait_for()
+        page.locator("Log In:).wait_for()
         page.get_by_text("Log In").click()
    
         page.screenshot(path=f'example-{browser_type.name}.png')
